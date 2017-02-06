acae7bc832838f66abf99c92b4cfdecffbe9a490
Connor Wyatt <connorwyatt1@gmail.com>
2017-02-06 00:27:35 +0000

    refactor(format): refactor formatting code to make better use of rxjs operators

--- tools/formatFiles.ts
+++ tools/formatFiles.ts
@@ -3,8 +3,10 @@
 import {formatFiles, IFileFormatResult} from './utils/formatUtils';
 import {replaceFileContents} from './utils/fileUtils';
 
-formatFiles('./!(node_modules)/**/!(*.d).ts').subscribe((fileFormatResult: IFileFormatResult) => {
-  if (fileFormatResult.currentFile != fileFormatResult.formattedFile) {
-    replaceFileContents(fileFormatResult.filePath, fileFormatResult.formattedFile);
-  }
-});
+formatFiles('./!(node_modules)/**/!(*.d).ts')
+    .filter((fileFormatResult: IFileFormatResult) => {
+      return fileFormatResult.currentFile != fileFormatResult.formattedFile;
+    })
+    .subscribe((fileFormatResult: IFileFormatResult) => {
+      replaceFileContents(fileFormatResult.filePath, fileFormatResult.formattedFile);
+    });
